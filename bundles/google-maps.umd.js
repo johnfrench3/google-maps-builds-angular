0a9ecc09ff35b918c42e4c5516e6af164325abd4
John French <johnfrench093@gmail.com>
2020-04-23 14:29:19 +0000

    master - 4eef95874 fix(autocomplete/testing): incorrect options if multiple panels are on the page at the same time (#19114)

--- bundles/google-maps.umd.js
+++ bundles/google-maps.umd.js
@@ -1070,7 +1070,11 @@
             this._eventManager.destroy();
             this._destroy.next();
             this._destroy.complete();
-            this.close();
+            // If no info window has been created on the server, we do not try closing it.
+            // On the server, an info window cannot be created and this would cause errors.
+            if (this.infoWindow) {
+                this.close();
+            }
         };
         /**
          * See developers.google.com/maps/documentation/javascript/reference/info-window#InfoWindow.close
