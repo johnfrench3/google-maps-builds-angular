f007dd835719e8747300f74f1fe97375feafcd45
John French <johnfrench093@gmail.com>
2020-06-15 18:03:41 +0000

    master - b089abe60 chore: bump version to 10.0.0-rc.2 w/ changelog (#19651)

--- google-map/google-map.d.ts
+++ google-map/google-map.d.ts
@@ -217,7 +217,7 @@ export declare class GoogleMap implements OnChanges, OnInit, OnDestroy {
      * See
      * https://developers.google.com/maps/documentation/javascript/reference/map#Map.controls
      */
-    get controls(): Array<google.maps.MVCArray<Node>>;
+    get controls(): google.maps.MVCArray<Node>[];
     /**
      * See
      * https://developers.google.com/maps/documentation/javascript/reference/map#Map.data
